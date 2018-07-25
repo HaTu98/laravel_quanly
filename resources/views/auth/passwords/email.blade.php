5aab7fea686c33a42fc83d5096ed903c63745d81
HaTu98 <hatu98nd@gmail.com>
2018-07-25 17:06:34 +0700

    fix bug reset password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -20,11 +20,7 @@
                 @endif
                 </div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    
 
                     <form method="POST" action="{{ route('password.email') }}" aria-label="{{ __('Reset Password') }}">
                         @csrf
